e1ba6b344b2257bbd3163f06bd0c72ce3b3b8f16
liamzwbao <zhiv.bao@gmail.com>
2023-01-19 11:45:05 -0800

    Implement wikimedia producer

--- kafka-producer-wikimedia/src/main/java/edu/neu/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/edu/neu/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,7 +1,38 @@
 package edu.neu.kafka.wikimedia;
 
-public class WikimediaChangesProducer {
-    public static void main(String[] args) {
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WikimediaChangesProducer {
+    public static void main(String[] args) throws InterruptedException {
+        String bootstrapServers = "localhost:9092";
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia.recentchange";
+
+        EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        try (EventSource eventSource = new EventSource.Builder(eventHandler, URI.create(url)).build()) {
+            // start the producer in another thread
+            eventSource.start();
+        }
+
+        // produce for 10 minutes and block the program until then
+        TimeUnit.MINUTES.sleep(10);
     }
 }
